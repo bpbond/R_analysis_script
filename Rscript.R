f3622a9b1b0c6c402b667504cb936fea714cfb11
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2015-02-26 18:00:08 -0500

    Added script template

---
+++ Rscript.R
@@ -0,0 +1,131 @@
+# Template for R analysis script
+# Ben Bond-Lamberty February 2015
+
+# This is my starting point for most analysis or data processing scripts
+# Most critically, it provides logging, with sessionInfo()  written at the 
+# bottom of every log; easy ggplot saving; logged csv read/write; and a
+# few other handy things.
+
+SCRIPTNAME		<- "Rscript.R"
+OUTPUT_DIR		<- "outputs/"
+RANDOM_SEED		<- 12345		# comment out to not set seed
+SEPARATOR		<- "-------------------"
+
+# -----------------------------------------------------------------------------
+# Time-stamped output function
+printlog <- function(msg="", ..., ts=TRUE, cr=TRUE) {
+	if(ts) cat(date(), " ")
+	cat(msg, ...)
+	if(cr) cat("\n")
+} # printlog
+
+# -----------------------------------------------------------------------------
+# Print dimensions of data frame
+print_dims <- function(d, dname=deparse(substitute(d))) {
+	stopifnot(is.data.frame(d))
+	printlog(dname, "rows =", nrow(d), "cols =", ncol(d))
+} # print_dims
+
+# -----------------------------------------------------------------------------
+# Return matrix of memory consumption
+object_sizes <- function() {
+    rev(sort(sapply(ls(envir=.GlobalEnv), function(object.name) 
+        object.size(get(object.name)))))
+} # object_sizes
+
+# -----------------------------------------------------------------------------
+# Return output directory (perhaps inside a script-specific folder)
+# If caller species `scriptfolder=FALSE`, return OUTPUT_DIR
+# If caller species `scriptfolder=TRUE` (default), return OUTPUT_DIR/SCRIPTNAME
+outputdir <- function(scriptfolder=TRUE) {
+    output_dir <- OUTPUT_DIR
+    if(scriptfolder) output_dir <- file.path(output_dir, sub(".R$", "", SCRIPTNAME))
+    if(!file.exists(output_dir)) dir.create(output_dir)
+    output_dir
+} # outputdir
+
+# -----------------------------------------------------------------------------
+# Save a ggplot figure
+save_plot <- function(pname, p=last_plot(), ptype=".pdf", scriptfolder=TRUE, ...) {
+	fn <- file.path(outputdir(scriptfolder), paste0(pname, ptype))
+	printlog("Saving", fn)
+	ggsave(fn, p, ...)
+} # save_plot
+
+# -----------------------------------------------------------------------------
+# Save a data frame
+save_data <- function(df, fname=paste0(deparse(substitute(df)), ".csv"), scriptfolder=TRUE, ...) {
+    fn <- file.path(outputdir(scriptfolder), fname)
+	printlog("Saving", fn)
+	write.csv(df, fn, row.names=F, ...)
+} # save_data
+
+# -----------------------------------------------------------------------------
+# Open a netCDF file and return handle (using ncdf4 package)
+open_ncdf <- function(fn, datadir=".") {
+	fqfn <- file.path(datadir, fn)
+	printlog("Opening", fqfn)
+	stopifnot(file.exists(fqfn))
+	nc_open(fqfn)
+} # open_ncdf
+
+# -----------------------------------------------------------------------------
+# Open a csv file and return data
+read_csv <- function(fn, datadir=".", ...) {
+	fqfn <- file.path(datadir, fn)
+	printlog("Opening", fqfn)
+	stopifnot(file.exists(fqfn))
+	read.csv(fqfn, stringsAsFactors=F, ...)
+} # read_csv
+
+# -----------------------------------------------------------------------------
+# Read data from the clipboard
+paste_data <- function(header=TRUE) {
+	read.table(pipe("pbpaste"), header=header)
+} # paste_data
+
+# -----------------------------------------------------------------------------
+is_outlier <- function(x, devs=3.2) {
+    # See: Davies, P.L. and Gather, U. (1993).
+    # "The identification of multiple outliers" (with discussion)
+    # J. Amer. Statist. Assoc., 88, 782-801.
+    
+    x <- na.omit(x)
+    lims <- median(x) + c(-1, 1) * devs * mad(x, constant = 1)
+    x < lims[ 1 ] | x > lims[2]
+} # is_outlier
+
+if(!file.exists(OUTPUT_DIR)) {
+	printlog("Creating", OUTPUT_DIR)
+	dir.create(OUTPUT_DIR)
+}
+
+
+# ==============================================================================
+# Main (or in a separate script that sources this one)
+
+sink(file.path(outputdir(), paste0(SCRIPTNAME, ".log.txt")), split=T)
+
+printlog("Welcome to", SCRIPTNAME)
+
+if(exists("RANDOM_SEED")) {
+	printlog("Setting random generator seed to", RANDOM_SEED)
+	set.seed(RANDOM_SEED)
+}
+
+library(ggplot2)
+theme_set(theme_bw())
+library(reshape2)
+library(dplyr)
+library(ncdf4)
+
+# ----- Main script goes here...
+
+# expression(SR[annual]~(g~C~m^{-2}~yr^{-1}))
+# text(x, y, bquote(R^{2} == .(r2)))
+# expression(italic(T)[5]~group("(",paste(degree,C),")"))
+
+
+printlog("All done with", SCRIPTNAME)
+print(sessionInfo())
+sink()
