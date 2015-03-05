4a5d26cd569c4a57c7ee6ee9544cec6d1e8876a8
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2015-03-05 16:21:42 -0500

    Reformatted a few things

--- Rscript.R
+++ Rscript.R
@@ -2,9 +2,9 @@
 # Ben Bond-Lamberty March 2015
 
 # This is my starting point for most analysis or data processing scripts
-# Most critically, it provides logging, with sessionInfo() written at the 
-# bottom of every log; easy ggplot saving; logged csv[.gz|zip] read/write;
-# and a few other handy things.
+# Most critically, it provides lightweight logging, with sessionInfo() 
+# written at the bottom of every log; easy ggplot and data saving; 
+# logged csv[.gz|zip] read/write; and a few other handy things.
 
 SCRIPTNAME		<- "Rscript.R"
 OUTPUT_DIR		<- "outputs/"
@@ -15,16 +15,16 @@ SEPARATOR		<- "-------------------"
 # -----------------------------------------------------------------------------
 # Time-stamped output function
 printlog <- function(msg="", ..., ts=TRUE, cr=TRUE) {
-	if(ts) cat(date(), " ")
-	cat(msg, ...)
-	if(cr) cat("\n")
+    if(ts) cat(date(), " ")
+    cat(msg, ...)
+    if(cr) cat("\n")
 } # printlog
 
 # -----------------------------------------------------------------------------
 # Print dimensions of data frame
 print_dims <- function(d, dname=deparse(substitute(d))) {
-	stopifnot(is.data.frame(d))
-	printlog(dname, "rows =", nrow(d), "cols =", ncol(d))
+    stopifnot(is.data.frame(d))
+    printlog(dname, "rows =", nrow(d), "cols =", ncol(d))
 } # print_dims
 
 # -----------------------------------------------------------------------------
@@ -48,44 +48,44 @@ outputdir <- function(scriptfolder=TRUE) {
 # -----------------------------------------------------------------------------
 # Save a ggplot figure
 save_plot <- function(pname, p=last_plot(), ptype=".pdf", scriptfolder=TRUE, ...) {
-	fn <- file.path(outputdir(scriptfolder), paste0(pname, ptype))
-	printlog("Saving", fn)
-	ggsave(fn, p, ...)
+    fn <- file.path(outputdir(scriptfolder), paste0(pname, ptype))
+    printlog("Saving", fn)
+    ggsave(fn, p, ...)
 } # save_plot
 
 # -----------------------------------------------------------------------------
 # Save a data frame
 save_data <- function(df, fname=paste0(deparse(substitute(df)), ".csv"), scriptfolder=TRUE, ...) {
     fn <- file.path(outputdir(scriptfolder), fname)
-	printlog("Saving", fn)
-	write.csv(df, fn, row.names=F, ...)
+    printlog("Saving", fn)
+    write.csv(df, fn, row.names=F, ...)
 } # save_data
 
 # -----------------------------------------------------------------------------
 # Open a netCDF file and return handle (using ncdf4 package)
 open_ncdf <- function(fn, datadir=".") {
-	fqfn <- file.path(datadir, fn)
-	printlog("Opening", fqfn)
-	nc_open(fqfn)
+    fqfn <- file.path(datadir, fn)
+    printlog("Opening", fqfn)
+    nc_open(fqfn)
 } # open_ncdf
 
 # -----------------------------------------------------------------------------
 # Open a (possibly compressed) csv file and return data
 read_csv <- function(fn, datadir=".", ...) {
-	fqfn <- file.path(datadir, fn)
-	printlog("Opening", fqfn)
-	if(grepl(".gz$", fqfn)) 
-		fqfn <- gzfile(fqfn)
-	else 
-		if(grepl(".zip$", fqfn)) 
-			fqfn <- unz(fqfn)
-	invisible(read.csv(fqfn, stringsAsFactors=F, ...))
+    fqfn <- file.path(datadir, fn)
+    printlog("Opening", fqfn)
+    if(grepl(".gz$", fqfn)) {
+        fqfn <- gzfile(fqfn)
+    } else if(grepl(".zip$", fqfn)) {
+        fqfn <- unz(fqfn)
+    }
+    invisible(read.csv(fqfn, stringsAsFactors=F, ...))
 } # read_csv
 
 # -----------------------------------------------------------------------------
 # Read data from the clipboard
 paste_data <- function(header=TRUE) {
-	read.table(pipe("pbpaste"), header=header)
+    read.table(pipe("pbpaste"), header=header)
 } # paste_data
 
 # -----------------------------------------------------------------------------
@@ -99,41 +99,36 @@ is_outlier <- function(x, devs=3.2) {
     x < lims[ 1 ] | x > lims[2]
 } # is_outlier
 
-
-# ==============================================================================
-
-
 if(!file.exists(OUTPUT_DIR)) {
-	printlog("Creating", OUTPUT_DIR)
-	dir.create(OUTPUT_DIR)
+    printlog("Creating", OUTPUT_DIR)
+    dir.create(OUTPUT_DIR)
 }
 
 
 # ==============================================================================
 # Main (or more commonly, put in a separate script that sources this one)
 
+# Setup, packages, reproducibility
 sink(file.path(outputdir(), paste0(SCRIPTNAME, ".log.txt")), split=T) # open log
 
 printlog("Welcome to", SCRIPTNAME)
 
 if(exists("RANDOM_SEED")) {
-	printlog("Setting random generator seed to", RANDOM_SEED)
-	set.seed(RANDOM_SEED)
+    printlog("Setting random generator seed to", RANDOM_SEED)
+    set.seed(RANDOM_SEED)
 }
 
-# -----------------------------------------------------------------------------
-# Packages and reproducibility
-
 if(require(checkpoint) & exists("CHECKPOINTDATE"))
-	try(checkpoint(CHECKPOINTDATE))
+    try(checkpoint(CHECKPOINTDATE)) # 'try' b/c errors w/o network (issue #171)
 library(ggplot2)
 theme_set(theme_bw())
 library(reshape2)
 library(dplyr)
-library(ncdf4)
+#library(ncdf4)
 
 # ----- Main script goes here...
 
+# Expressions...
 # expression(SR[annual]~(g~C~m^{-2}~yr^{-1}))
 # text(x, y, bquote(R^{2} == .(r2)))
 # expression(italic(T)[5]~group("(",paste(degree,C),")"))
